f09778fca00ebca31c60d2751e3211fd34e9e9db
DarkBest71 <17887924+DarkBest71@users.noreply.github.com>
2022-06-04 23:30:08 +0200

    Update messageUpdate.js

--- messageUpdate.js
+++ messageUpdate.js
@@ -1,27 +1,19 @@
-const Discord = require("discord.js");
-const db = require("quick.db");
-const config = require("../../jsons/config.json");
+const { MessageEmbed } = require("discord.js");
+const moment = require('moment');
 
-module.exports = { run: async(client, oldMessage, newMessage ) => {
-    if(oldMessage.author.bot) return;
-    if(oldMessage.channel.type === "dm") return;
-    if(oldMessage.author.bot) return;
-    if(oldMessage.content === newMessage.content) return;
-    if(newMessage.content.length > 250) return;
-    let channel = db.get("logs_channel_" + oldMessage.guild.id)
-    if(client.channels.cache.get(channel)) { 
-    
-    const embed = new Discord.MessageEmbed()
-    .setThumbnail(client.user.avatarURL)
-    .setAuthor({name: `Exécutée par ${oldMessage.author.username}`, iconURL: `${oldMessage.guild.iconURL({dynamic: true})}`})
-    .setTitle(`Message modifié **ON** dans #${oldMessage.channel.name} :`)
-    .addField("Ancien message :", oldMessage.content)
-    .addField("Nouveau message :", newMessage.content)
-    .setColor("#fc3c3c")
-    .setFooter({text:"Modiffication du message part " + oldMessage.author.tag})
-    .setTimestamp()
-    return client.channels.cache.get(channel).send({ embeds: [embed] })
-}
-},
-name: 'messageUpdate'
-};
+module.exports = {
+    run: (client, member, message) => {
+        const channel = member.guild.channels.cache.get('ID Salon');
+
+        member.roles.add("579261689111904306").then(console.log('Role added: ' + member.id));
+        const embedNewMember = new MessageEmbed()
+        .setColor('BLUE')
+        .setDescription(`Bienvenue à ${member} sur ${member.guild.name}.`)
+        .setAuthor({name: `Nouveau membre`, iconURL: `${member.guild.iconURL({dynamic: true})}`})
+        .addField('Création du compte:', moment(member.user.createdAt).format('L'))
+        .addField('A rejoint le:', moment(member.joinedAt).format('L'))
+        .setFooter({text:` ${member.guild.memberCount} sur le serveur ! `})
+        channel.send({embeds: [embedNewMember]})
+    },
+    name: 'guildMemberAdd'
+} 
